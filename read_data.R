84b4963a1fee632fb7563994ee41b43bad3e4eb5
Zulma M Cucunubá <zulma.cucunuba@javeriana.edu.co>
2023-10-10 15:33:43 -0500

    mapping

--- read_data.R
+++ read_data.R
@@ -1,4 +1,11 @@
 
 
+# Other packages to check
+
+# https://cran.r-project.org/src/contrib/Archive/charlson/
+# https://github.com/ellessenne/comorbidity/
+# https://rpubs.com/nathanh36/jjcdm
+
+
 dat <- read(base_de_prueba)
 
