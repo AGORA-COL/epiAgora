e1a3f474b306d0f1b6b4f9d3319104c7cbd3aef7
Johan Calderón <dani@danielabemo>
2024-02-23 12:18:11 -0500

    output folder

--- scripts/consolidado.R
+++ scripts/consolidado.R
@@ -1,6 +1,7 @@
 # Rutina para clasificación de enfermedades
 # Fecha: DIC-2023
 # Proyecto AGORA
+#===============================================================================
 
 #cargar paquetes
 library(tidyverse)
@@ -15,6 +16,7 @@ source("fun/class_alg.R")
 
 diseases <- c("DM", "ERC")
 
+#===============================================================================
 #Cargar datos
 #CÓDIGOS DM
 cods_DM <- read_excel("dat/ALGORITMOS_CLINICOS_NEW_ZC.xlsx", sheet = diseases[1])
@@ -44,7 +46,7 @@ bd2015 <- read_excel("dat/bd_2015.xlsx")
 names(bd2015) <- epitrix::clean_labels(names(bd2015))
 bd2015 <- bd2015 %>% mutate (name = epitrix::clean_labels(medicamento))
 
-#consolidado
+#crear database "consolidado" en blanco
 consolidado <- data.frame(enf = character(),
                           bd = character(),
                           muestra = numeric(),
@@ -59,15 +61,16 @@ consolidado <- data.frame(enf = character(),
                           prev_literatura = numeric()
 )
 
-#EVIDENCIA
+#Leer datos de EVIDENCIA
 evidencia <- read_excel("dat/validacion.xlsx")
 
-
+#===============================================================================
 #Clasificación DM
 bd2011 <- classification(cods_DM,bd2011)
 bd2012 <- classification(cods_DM,bd2012)
 bd2014 <- classification(cods_DM,bd2014)
 bd2015 <- classification(cods_DM,bd2015)
+
 #Creación tabla resultados
 disease <- diseases[1]
 evidencia_DM <- evidencia %>% filter(enfermedad == "DM")
@@ -76,19 +79,23 @@ resultados_tabla<-rbind(resultados(bd2011, disease), resultados(bd2012, disease)
 resultados_tabla <- resultados_tabla %>% mutate(prev_literatura = evidencia_DM$prevalencia)
 consolidado <- rbind (consolidado, resultados_tabla)
 
-
+#===============================================================================
 #Clasificación ERC
 bd2011 <- classification(cods_ERC,bd2011)
 bd2012 <- classification(cods_ERC,bd2012)
 bd2014 <- classification(cods_ERC,bd2014)
 bd2015 <- classification(cods_ERC,bd2015)
+
 #Creación tabla resultados
 disease <- diseases[2]
 evidencia_ERC <- evidencia %>% filter(enfermedad == "ERC")
 
-resultados_tabla<-rbind(resultados(bd2011, disease), resultados(bd2012, disease),
+resultados_tabla <- rbind(resultados(bd2011, disease), resultados(bd2012, disease),
                         resultados(bd2014, disease),resultados(bd2015, disease))
 resultados_tabla <- resultados_tabla %>% mutate(prev_literatura = evidencia_ERC$prevalencia)
 
 #Consolidado
 consolidado <- rbind (consolidado, resultados_tabla)
+
+
+saveRDS
